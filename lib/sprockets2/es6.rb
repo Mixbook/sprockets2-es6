7c239c7311a4c3f3a1d86bf740d09aad7eec1f68
Dan Schultz <schultz.t.dan@gmail.com>
2016-05-10 17:46:02 -0700

    Embed the processor in an ES6 module

--- lib/sprockets2/es6.rb
+++ lib/sprockets2/es6.rb
@@ -4,62 +4,64 @@ require 'sprockets2/es6/version'
 require 'tilt'
 
 module Sprockets2
-  class ES6Processor < Tilt::Template
-    self.default_mime_type = 'application/javascript'
+  module ES6
+    class Processor < Tilt::Template
+      self.default_mime_type = 'application/javascript'
 
-    # Configuration options that are passed to the Babel transpiler. See
-    # https://developit.github.io/babel-legacy-docs//docs/usage/options/
-    # for possible options that can be set.
-    #
-    # Since Sprockets 2 expects a class reference when adding the engine,
-    # you'll need to create a subclass to override the configuration.
-    def configuration
-      {}
-    end
-
-    def prepare
-      # Noop. Tilt expects this to be overridden, even if it doesn't do anything.
-    end
-
-    def evaluate(context, locals, &block)
-      opts = transformation_options(context)
-      result = transform(data, opts)
-      "#{result['code']}\n\n"
-    end
-
-    def transform(data, opts)
-      Babel::Transpiler.transform(data, opts)
-    end
-
-    def transformation_options(context)
-      opts = configuration.merge({
-        'filename' => file,
-        'filenameRelative' => split_subpath(context.root_path, file),
-        'sourceRoot' => context.root_path
-      })
-
-      if opts['moduleIds'] && opts['moduleRoot']
-        opts['moduleId'] ||= File.join(opts['moduleRoot'], context.logical_path)
-      elsif opts['moduleIds']
-        opts['moduleId'] ||= context.logical_path
+      # Configuration options that are passed to the Babel transpiler. See
+      # https://developit.github.io/babel-legacy-docs//docs/usage/options/
+      # for possible options that can be set.
+      #
+      # Since Sprockets 2 expects a class reference when adding the engine,
+      # you'll need to create a subclass to override the configuration.
+      def configuration
+        {}
       end
 
-      opts
-    end
+      def prepare
+        # Noop. Tilt expects this to be overridden, even if it doesn't do anything.
+      end
 
-    private
+      def evaluate(context, locals, &block)
+        opts = transformation_options(context)
+        result = transform(data, opts)
+        "#{result['code']}\n\n"
+      end
 
-    def load_path(logical_path, pathname)
-      pathname.to_s.replace(logical_path)
-    end
+      def transform(data, opts)
+        Babel::Transpiler.transform(data, opts)
+      end
 
-    def split_subpath(path, subpath)
-      return "" if path == subpath
-      path = File.join(path, '')
-      if subpath.start_with?(path)
-        subpath[path.length..-1]
-      else
-        nil
+      def transformation_options(context)
+        opts = configuration.merge({
+          'filename' => file,
+          'filenameRelative' => split_subpath(context.root_path, file),
+          'sourceRoot' => context.root_path
+        })
+
+        if opts['moduleIds'] && opts['moduleRoot']
+          opts['moduleId'] ||= File.join(opts['moduleRoot'], context.logical_path)
+        elsif opts['moduleIds']
+          opts['moduleId'] ||= context.logical_path
+        end
+
+        opts
+      end
+
+      private
+
+      def load_path(logical_path, pathname)
+        pathname.to_s.replace(logical_path)
+      end
+
+      def split_subpath(path, subpath)
+        return "" if path == subpath
+        path = File.join(path, '')
+        if subpath.start_with?(path)
+          subpath[path.length..-1]
+        else
+          nil
+        end
       end
     end
   end
